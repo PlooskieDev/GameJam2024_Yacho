5d6d5a2b686552fa0b51aedfb2122c2e8500cb1c
Tomas Pavlik <tpavlik@monetplus.cz>
2024-03-08 23:45:11 +0100

    Movement fix, jumping

---
+++ Assets/Script/PickupController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PickupController : MonoBehaviour {
+    //Add comment to a script
+    [TextArea(1, 5)]
+    public string Notes = "Comment";
+
+    //--------------------------------------------------------------------------------------------------------------------------
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+}//END
