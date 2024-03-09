6532be9fd859209c6bda16e0f54ad6df76cf066f
Michael Pluskal <misplus@volny.cz>
2024-03-09 13:32:16 +0100

    Main sync

---
+++ Assets/Script/LightController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+    //Add comment to a script
+    [TextArea(1, 5)]
+    public string Notes = "Comment";
+
+    [SerializeField]
+    Light light;
+
+    //--------------------------------------------------------------------------------------------------------------------------
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        
+    }
+
+}//END
